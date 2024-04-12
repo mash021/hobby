9db62a85e2592830d90d1d595474e58115d78026
AliSharifi <mohammadali.sharifi@rocketmail.com>
2024-04-12 15:45:51 +0200

    play note

--- signalGenerato.py
+++ signalGenerato.py
@@ -2,12 +2,12 @@ import pyaudio
 import numpy as np
 import tkinter as tk
 
-# تنظیمات صوتی
+# تنظیمات صوتی setting Audio
 SAMPLE_RATE = 32000
 BUFFER_SIZE = 256
 
 
-# تعریف موج‌ها
+# تعریف موج‌ها define wave type
 class WaveGenerator:
     def __init__(self, wave_type):
         self.wave_type = wave_type
@@ -20,3 +20,23 @@ class WaveGenerator:
             return np.abs(2 * np.pi * frequency * t - 2 * np.pi * frequency * t)
         elif self.wave_type == 'square':
             return np.sign(np.sin(2 * np.pi * frequency * t))
+        
+# تابع برای پخش نت (play note)
+def play_note():
+    frequency = float(note_frequency.get())
+    duration = float(note_duration.get())
+    wave_type = wave_type_var.get()
+
+    wave_generator = WaveGenerator(wave_type)
+    samples = wave_generator.generate_wave(frequency, duration)
+
+    # تعریف و شروع Stream
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paFloat32, channels=1, rate=SAMPLE_RATE, output=True)
+    stream.write(samples.astype(np.float32).tobytes())
+
+    # متوقف کردن و بستن Stream
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
